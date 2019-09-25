40da0e40340abe40a39281aed801e5e3bae8ce91
Waseef Shawkat <awshawka@uwaterloo.ca>
2019-09-24 21:22:50 -0400

    added the graph

--- js/background.js
+++ js/background.js
@@ -1,31 +1,49 @@
 // fired when message is received from content_script
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   chrome.storage.local.get({ ticker: [] }, function(res) {
-    let history = res.ticker == null ? [] : res.ticker;
-    let found = false;
+    if (sender.tab.title != undefined) {
+      let history = res.ticker == null ? [] : res.ticker;
+      let found = false;
 
-    for (let i = 0; i < history.length; i++) {
-      if (history[i].icon == sender.tab.favIconUrl) {
-        history[i].count += 1;
-        found = true;
-        break;
+      for (let i = 0; i < history.length; i++) {
+        if (history[i].icon == sender.tab.favIconUrl) {
+          history[i].count += 1;
+          found = true;
+          break;
+        }
       }
-    }
 
-    if (!found) {
-      history.push({
-        title: sender.tab.title,
-        icon: sender.tab.favIconUrl,
-        count: 1
+      if (!found) {
+        history.push({
+          title: sender.tab.title,
+          icon: sender.tab.favIconUrl,
+          count: 1
+        });
+      }
+
+      chrome.storage.local.set({ ticker: history });
+      chrome.storage.local.get(["total_time"], function(time) {
+        chrome.storage.local.set({
+          total_time: time.total_time == null ? 1 : time.total_time + 1
+        });
       });
     }
+  });
 
-    chrome.storage.local.set({ ticker: history });
-    chrome.storage.local.get(["total_time"], function(time) {
-      chrome.storage.local.set({
-        total_time: time.total_time == null ? 1 : time.total_time + 1
-      });
-    });
+  chrome.storage.local.get({ hours: [] }, function(res) {
+    let timeSpent = res.hours == null ? [] : res.hours;
+
+    // increment the count for the hour
+    if (timeSpent.length == 0) {
+      for (let i = 0; i < 24; i++) {
+        timeSpent.push(0);
+      }
+    } else {
+      timeSpent[new Date().getHours()]++;
+    }
+
+    console.log(timeSpent);
+    chrome.storage.local.set({ hours: timeSpent });
   });
 });
 
